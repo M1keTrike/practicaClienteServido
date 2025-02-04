d5d166f35118e5c437e15f87faabc9b150aaaef7
M1keTrike <miguemolina4570@gmail.com>
2025-02-04 12:37:06 -0600

    [ADDED] replication server integration with main server for replica

--- mainServer/main.go
+++ mainServer/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/M1keTrike/practicaClienteServido/entities"
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	users  []entities.User
-	mutex  sync.Mutex
-	nextID = 1
+	users         []entities.User
+	mutex         sync.Mutex
+	nextID        = 1
+	insertedUsers []entities.User
+	updatedUsers  []entities.User
+	deletedUsers  []int
 )
+
 
 func createUser(c *gin.Context) {
 	var newUser entities.User
@@ -25,12 +31,15 @@ func createUser(c *gin.Context) {
 	mutex.Lock()
 	newUser.Id = nextID
 	nextID++
+	newUser.OperationStatus = "INSERT"
 	users = append(users, newUser)
+	insertedUsers = append(insertedUsers, newUser) 
+
+
 	mutex.Unlock()
 
 	c.JSON(http.StatusCreated, newUser)
 }
-
 
 
 func updateUserByID(c *gin.Context) {
@@ -52,7 +61,9 @@ func updateUserByID(c *gin.Context) {
 	for i, user := range users {
 		if user.Id == id {
 			updatedUser.Id = id
+			updatedUser.OperationStatus = "UPDATE"
 			users[i] = updatedUser
+			updatedUsers = append(updatedUsers, updatedUser) 
 			c.JSON(http.StatusOK, updatedUser)
 			return
 		}
@@ -74,6 +85,7 @@ func deleteUserByID(c *gin.Context) {
 	for i, user := range users {
 		if user.Id == id {
 			users = append(users[:i], users[i+1:]...)
+			deletedUsers = append(deletedUsers, id) 
 			c.JSON(http.StatusOK, gin.H{"message": "Usuario eliminado"})
 			return
 		}
@@ -82,14 +94,65 @@ func deleteUserByID(c *gin.Context) {
 	c.JSON(http.StatusNotFound, gin.H{"error": "Usuario no encontrado"})
 }
 
+func checkChanges(c *gin.Context) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	hasChanges := len(insertedUsers) > 0 || len(updatedUsers) > 0 || len(deletedUsers) > 0
+
+	c.JSON(http.StatusOK, gin.H{"changes": hasChanges})
+}
+
+func longPollingReplication(c *gin.Context) {
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.Header().Set("Transfer-Encoding", "chunked")
+
+	for {
+		time.Sleep(2 * time.Second)
+
+		mutex.Lock()
+		if len(insertedUsers) > 0 {
+			user := insertedUsers[0]
+			insertedUsers = insertedUsers[1:] 
+			mutex.Unlock()
+
+		
+			json.NewEncoder(c.Writer).Encode(user)
+			c.Writer.Flush()
+			return 
+		}
+		mutex.Unlock()
+	}
+}
+
+
+func getUpdatedUsers(c *gin.Context) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	c.JSON(http.StatusOK, updatedUsers)
+	updatedUsers = []entities.User{}
+}
+
+func getDeletedUsers(c *gin.Context) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	c.JSON(http.StatusOK, deletedUsers)
+	deletedUsers = []int{}
+}
+
 func main() {
 	r := gin.Default()
 
-	
-	r.POST("/users", createUser)      
-
-	r.PUT("/users/:id", updateUserByID) 
+	r.POST("/users", createUser)
+	r.PUT("/users/:id", updateUserByID)
 	r.DELETE("/users/:id", deleteUserByID)
 
-	r.Run(":8081") 
+	r.GET("/changes", checkChanges)
+	r.GET("/longpolling", longPollingReplication)
+	r.GET("/updated-users", getUpdatedUsers)
+	r.GET("/deleted-users", getDeletedUsers)
+
+	r.Run(":8081")
 }
--- repliaction/main.go
+++ repliaction/main.go
@@ -1,21 +1,128 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
 	"github.com/M1keTrike/practicaClienteServido/entities"
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	replicatedUsers []entities.User
+	mutex           sync.Mutex
+)
 
-type Strorage struct {
-	Users []entities.User
+func checkForChanges() {
+	for {
+		resp, err := http.Get("http://localhost:8081/changes")
+		if err != nil {
+			fmt.Println("Error en short polling:", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		var result map[string]bool
+		json.NewDecoder(resp.Body).Decode(&result)
+		resp.Body.Close()
+
+		if result["changes"] {
+			
+			go receiveNewRecords()
+
+			
+			getUpdatedRecords()
+
+		
+			getDeletedRecords()
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
 
 
+func receiveNewRecords() {
+	for {
+		resp, err := http.Get("http://localhost:8081/longpolling")
+		if err != nil {
+			fmt.Println("Error en long polling:", err)
+			time.Sleep(2 * time.Second) 
+			continue
+		}
 
+		decoder := json.NewDecoder(resp.Body)
+		var user entities.User
+		if err := decoder.Decode(&user); err == nil {
+			mutex.Lock()
+			replicatedUsers = append(replicatedUsers, user)
+			mutex.Unlock()
+			fmt.Println("Nuevo usuario replicado:", user)
+		}
+
+		resp.Body.Close()
+	}
+}
+
+
+func getUpdatedRecords() {
+	resp, err := http.Get("http://localhost:8081/updated-users")
+	if err != nil {
+		fmt.Println("Error obteniendo actualizaciones:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var updatedUsers []entities.User
+	json.NewDecoder(resp.Body).Decode(&updatedUsers)
+
+	mutex.Lock()
+	for i, user := range replicatedUsers {
+		for _, updated := range updatedUsers {
+			if user.Id == updated.Id {
+				replicatedUsers[i] = updated
+			}
+		}
+	}
+	mutex.Unlock()
+}
+
+
+func getDeletedRecords() {
+	resp, err := http.Get("http://localhost:8081/deleted-users")
+	if err != nil {
+		fmt.Println("Error obteniendo eliminaciones:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var deletedIDs []int
+	json.NewDecoder(resp.Body).Decode(&deletedIDs)
+
+	mutex.Lock()
+	for _, id := range deletedIDs {
+		for i, user := range replicatedUsers {
+			if user.Id == id {
+				replicatedUsers = append(replicatedUsers[:i], replicatedUsers[i+1:]...)
+				break
+			}
+		}
+	}
+	mutex.Unlock()
+}
 
 func main() {
+	go checkForChanges()
+
 	r := gin.Default()
+	r.GET("/replicated-users", func(c *gin.Context) {
+		mutex.Lock()
+		defer mutex.Unlock()
+		c.JSON(http.StatusOK, replicatedUsers)
+	})
 
-
-	r.Run("8081")
+	r.Run(":8082")
 }
